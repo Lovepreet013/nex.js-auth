b05f274612baa6a2e3ce7280f64165a1cfffd7a6
Lovepreet013 <singhlovepreet1209@gmail.com>
2024-06-22 18:22:17 +0530

    icon added

---
+++ app/icon.tsx
@@ -0,0 +1,39 @@
+import { ImageResponse } from 'next/og'
+// Route segment config
+export const runtime = 'edge'
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+// Image generation
+export default function Icon(props: any) {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 24 24"
+      fill="white"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M12.67 19a2 2 0 0 0 1.416-.588l6.154-6.172a6 6 0 0 0-8.49-8.49L5.586 9.914A2 2 0 0 0 5 11.328V18a1 1 0 0 0 1 1z" />
+      <path d="M16 8 2 22" />
+      <path d="M17.5 15H9" />
+    </svg>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  )
+}
